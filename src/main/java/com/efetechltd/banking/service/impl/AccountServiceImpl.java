87ae456bdb4db178b07ec51d58afca6931a1904c
feisalmombo <feisalmombo29@gmail.com>
2024-09-02 16:27:09 +0300

    WIP

--- src/main/java/com/efetechltd/banking/service/impl/AccountServiceImpl.java
+++ src/main/java/com/efetechltd/banking/service/impl/AccountServiceImpl.java
@@ -3,13 +3,13 @@ package com.efetechltd.banking.service.impl;
 import org.springframework.stereotype.Service;
 
 import com.efetechltd.banking.dto.AccountDto;
+import com.efetechltd.banking.entity.Account;
+import com.efetechltd.banking.mapper.AccountMapper;
 import com.efetechltd.banking.repository.AccountRepository;
 import com.efetechltd.banking.service.AccountService;
 
-
 @Service
 public class AccountServiceImpl implements AccountService {
-
 
     private AccountRepository accountRepository;
 
@@ -19,7 +19,9 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public AccountDto createAccount(AccountDto accountDto) {
-        return null;
+        Account account = AccountMapper.mapToAccount(accountDto);
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
     }
 
 }
