6fec28eb63745293e20290cd91366186affa4ca4
feisalmombo <feisalmombo29@gmail.com>
2024-09-02 19:16:11 +0300

    WIP

--- src/main/java/com/efetechltd/banking/controller/AccountController.java
+++ src/main/java/com/efetechltd/banking/controller/AccountController.java
@@ -1,5 +1,14 @@
 package com.efetechltd.banking.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.efetechltd.banking.service.AccountService;
+
+@RestController
+@RequestMapping("/api/accounts")
 public class AccountController {
+
+    private AccountService accountService;
 
 }
