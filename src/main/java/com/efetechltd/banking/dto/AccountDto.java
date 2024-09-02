ae34d82f407f9e26d4fb98bb7e1fe7a53a928720
feisalmombo <feisalmombo29@gmail.com>
2024-09-02 15:36:27 +0300

    WIP

--- src/main/java/com/efetechltd/banking/dto/AccountDto.java
+++ src/main/java/com/efetechltd/banking/dto/AccountDto.java
@@ -1,5 +1,12 @@
 package com.efetechltd.banking.dto;
 
+import lombok.Data;
+
+@Data
 public class AccountDto {
+
+    private Long id;
+    private String accountHolderName;
+    private double balance;
 
 }
