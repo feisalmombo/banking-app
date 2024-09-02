c52048954fbbdb69709e2f94d730cc25d0ce696e
feisalmombo <feisalmombo29@gmail.com>
2024-09-02 16:18:38 +0300

    fix bugs

--- src/main/java/com/efetechltd/banking/mapper/AccountMapper.java
+++ src/main/java/com/efetechltd/banking/mapper/AccountMapper.java
@@ -1,5 +1,29 @@
 package com.efetechltd.banking.mapper;
 
+import com.efetechltd.banking.dto.AccountDto;
+import com.efetechltd.banking.entity.Account;
+
 public class AccountMapper {
+
+    public static Account mapToAccount(AccountDto accountDto) {
+        Account account =  new Account(
+            accountDto.getId(),
+            accountDto.getAccountHolderName(),
+            accountDto.getBalance()
+        );
+
+        return account;
+    }
+
+
+    public static AccountDto mapToAccountDto(Account account) {
+        AccountDto accountDto = new AccountDto(
+            account.getId(),
+            account.getAccountHolderName(),
+            account.getBalance()
+        );
+
+        return accountDto;
+    }
 
 }
