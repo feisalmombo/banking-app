6042c2e625172bda7f7400ebb0d701bf7418e2c5
feisalmombo <feisalmombo29@gmail.com>
2024-09-04 17:39:04 +0300

    WIP

--- src/main/java/com/efetechltd/banking/controller/AccountController.java
+++ src/main/java/com/efetechltd/banking/controller/AccountController.java
@@ -1,8 +1,11 @@
 package com.efetechltd.banking.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.efetechltd.banking.dto.AccountDto;
 import com.efetechltd.banking.service.AccountService;
 
 @RestController
@@ -11,4 +14,12 @@ public class AccountController {
 
     private AccountService accountService;
 
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    // Add Account REST API
+    public ResponseEntity<AccountDto> addAccount(AccountDto accountDto) {
+        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
 }
